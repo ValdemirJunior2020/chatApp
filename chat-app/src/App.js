be0b1e4e6cfaca1997d801a66260438ce5ece789
Valdemir rodrigues Goncalves Junior <72047928+ValdemirJunior2020@users.noreply.github.com>
2021-04-23 18:32:15 -0400

    Add files via upload

---
+++ chat-app/src/App.js
@@ -0,0 +1,18 @@
+import { ChatEngine } from 'react-chat-engine'
+
+const App = () => {
+
+
+    return ( <
+        ChatEngine height = "100vh"
+        projectID = "2d337831-3cca-4859-ac83-ca4f53d56c31"
+        userName = "sarandi123"
+        userSecret = "111111111"
+
+        /
+        >
+
+
+    );
+}
+export default App;
